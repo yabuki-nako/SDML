8db5f36c3f56657e9db14fcd295e7bd925655476
yabuki-nako <lesterlusung1414@gmail.com>
2024-05-02 02:46:10 +0800

    s

--- get_time.php
+++
@@ -1,6 +0,0 @@
-<?php
-  date_default_timezone_set('Asia/Manila'); // Set your desired timezone
-
-  $current_time = date('h:i a '); // Get the current time
-  echo $current_time; // Output the current time
-?>
